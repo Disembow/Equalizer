17ecf448858d0a919e83187cbcb1432d6b3af910
Disembow <e_naletov@mail.ru>
2024-01-30 11:36:11 +0100

    feat: add layout, getting audio src from input

--- src/js/Equalizer.controller.js
+++ src/js/Equalizer.controller.js
@@ -1,5 +1,13 @@
 import EqualizerModel from './Equalizer.model';
 
-class EqualizerController extends EqualizerModel {}
+class EqualizerController extends EqualizerModel {
+  getAudioSource() {
+    const audioPlayer = document.querySelector('.audio');
+    const input = document.querySelector('.input');
+
+    const audioFile = input.files[0];
+    audioPlayer.src = URL.createObjectURL(audioFile);
+  }
+}
 
 export default EqualizerController;
--- src/js/Equalizer.view.js
+++ src/js/Equalizer.view.js
@@ -3,9 +3,33 @@ import EqualizerController from './Equalizer.controller';
 
 class EqualizerView {
   #controller = new EqualizerController();
-  #root = document.querySelector('.app');
+  #root = document.getElementById('app');
+  #cellsX = 6;
+  #cellsY = 6;
 
-  #render() {}
+  #renderGrid() {
+    const wrapper = createTag('section', ['equilizer__wrapper'], this.#root);
+
+    for (let i = 0; i < this.#cellsY; i += 1) {
+      const row = createTag('div', ['row'], wrapper);
+
+      for (let j = 0; j < this.#cellsX; j += 1) {
+        createTag('div', ['cell'], row);
+      }
+    }
+  }
+
+  #render() {
+    this.#renderGrid();
+
+    createTag('audio', ['audio'], this.#root, {
+      controls: true,
+    });
+    createTag('input', ['input'], this.#root, {
+      type: 'file',
+      onchange: () => this.#controller.getAudioSource(),
+    });
+  }
 
   init() {
     this.#render();
